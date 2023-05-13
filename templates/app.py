221701bc7d971ce8a7b302b5d263c87f291a361a
adz912 <adelzaky912@gmail.com>
2023-05-13 19:40:40 +0100

    Website Deploys and Displays Properly
    Managed to get the CSS working and had to adjust a lot of the Flask and HTML files to ensure they were all directing to the right links. All pages are working in terms of linking to one another.

--- templates/app.py
+++ templates/app.py
@@ -25,5 +25,30 @@ def index():
     # Render the data on the web page
     return render_template('index.html', data=data)
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/pc')
+def pc():
+    return render_template('pc.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/comparison')
+def comparison():
+    return render_template('comparison.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
