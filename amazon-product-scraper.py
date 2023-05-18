caa7c457e168a4eb14130b5eac87de7a53126152
adz912 <adelzaky912@gmail.com>
2023-05-18 20:11:32 +0100

    Base Amazon Scraper Functionality
    The scraper for the Amazon pre built page now works as intended. Only the name and the price of the PCs are scraped.

--- amazon-product-scraper.py
+++ amazon-product-scraper.py
@@ -2,63 +2,81 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 import mysql.connector
+from mysql.connector import Error
 
-# Connect to MySQL database
-mydb = mysql.connector.connect(
-  host="ADELS-PC",
-  user="Admin",
-  password="Adminpw2023",
-  database="pc_comparison"
-)
-mycursor = mydb.cursor()
+# Specifys the URL of the Amazon page you want to scrape
+url = 'https://www.amazon.co.uk/s?k=gpu+graphics+card&crid=27S83A5E856GO&sprefix=gpu%2Caps%2C251&ref=nb_sb_ss_ts-doa-p_2_3'
 
+# Specifys the path to the chromedriver executable (you will need to download this)
+driver_path = 'C:/Users/Adel/OneDrive - City, University of London/Documents/Project/chromedriver.exe'
 
+# Configures the service with the executable path
+service = webdriver.chrome.service.Service(driver_path)
 
-# Set the path to the Chrome webdriver
-# Set the path to the Chrome webdriver
-# Set the path to the Chrome webdriver
-chrome_driver_path = "C:/Users/Adel/Downloads/chromedriver_win32/chromedriver.exe"
+# Create a new Chrome browser instance
+driver = webdriver.Chrome(service=service)
 
+# Navigates to the URL
+driver.get(url)
 
-# Launch Chrome driver
-options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
-driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
+# Wait for the page to load
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.s-result-item')))
 
-# Navigate to Amazon UK pre-built PC listings
-driver.get("https://www.amazon.co.uk/s?k=pre+built+pc&ref=nb_sb_noss")
+# Finds all the product elements on the page
+product_elements = driver.find_elements(By.CSS_SELECTOR, '.s-result-item')
 
-# Wait for page to load and accept cookies
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "sp-cc-accept"))).click()
-WebDriverWait(driver, 10).until_not(EC.presence_of_element_located((By.ID, "sp-cc-accept")))
+# Sets up the connection to your MySQL database
+try:
+    connection = mysql.connector.connect(
+        host="ADELS-PC",
+        user="Admin",
+        password="Adminpw2023",
+        database="pc_comparison"
+    )
 
-# Find all PC listings on page
-pc_listings = driver.find_elements(By.CSS_SELECTOR, "div[data-component-type='s-search-result']")
+    if connection.is_connected():
+        print('Connected to MySQL database')
+except Error as e:
+    print(f'Error while connecting to MySQL: {e}')
 
-# Loop through each listing and extract relevant information
-for listing in pc_listings:
-    # Extract product title
-    title_element = listing.find_element(By.CSS_SELECTOR, "h2 a")
-    title = title_element.text
-    
-    # Extract product price
+# Insert query
+insert_query = "INSERT INTO pcs (name, price) VALUES (%s, %s)"
+
+# Loops through the product elements and extract the name and price for each one
+for product_element in product_elements:
     try:
-        price_element = listing.find_element(By.CSS_SELECTOR, "span.a-price span.a-offscreen")
-        price = float(price_element.text.replace('£',''))
-    except:
-        price = None
-    
-    # Extract product link
-    link_element = listing.find_element(By.CSS_SELECTOR, "h2 a")
-    link = link_element.get_attribute("href")
-    
-    # Save data into MySQL database
-    sql = "INSERT INTO pcs (name, price, link) VALUES (%s, %s, %s)"
-    val = (title, price, link)
-    mycursor.execute(sql, val)
-    mydb.commit()
+        name_element = product_element.find_element(By.TAG_NAME, 'h2')
+        name = name_element.text
+    except NoSuchElementException:
+        name = "N/A"
 
-# Close driver and database connection
+    try:
+        price_element = product_element.find_element(By.CSS_SELECTOR, '.a-price-whole')
+        price = price_element.text
+    except NoSuchElementException:
+        price = "N/A"
+
+    # Handles the case where price is 'N/A'
+    if price == 'N/A':
+        price = 0  # Set a default price value or adjust it according to your needs
+
+    # Prints the name and price to the console
+    print(f'{name}: {price}')
+
+    # Insert the data into the database
+    data = (name, price)
+    cursor = connection.cursor()
+    cursor.execute(insert_query, data)
+    connection.commit()
+
+# Closes the browser
 driver.quit()
-mydb.close()
+
+# Closes the database connection
+if connection.is_connected():
+    cursor.close()
+    connection.close()
+    print('MySQL connection closed')
