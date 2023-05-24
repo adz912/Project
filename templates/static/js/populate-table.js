cfce8853719272b811820fa35da2e6889c5e99ea
adz912 <adelzaky912@gmail.com>
2023-05-24 01:01:27 +0100

    Added N/A and Changed styling
    Added N/A to HDD Storage when it is Null and changed the styling of the table

--- templates/static/js/populate-table.js
+++ templates/static/js/populate-table.js
@@ -13,24 +13,31 @@ $(document).ready(function() {
                 // Iterate over the data and create table rows
                 for (var i = 0; i < data.length; i++) {
                     var pc = data[i];
-                    var row = `<tr>
-                        <td>${pc.title}</td>
-                        <td>${pc.cpu}</td>
-                        <td>${pc.coreCount}</td>
-                        <td>${pc.cpuBoostClock}</td>
-                        <td>${pc.memory}</td>
-                        <td>${pc.ssdStorage}</td>
-                        <td>${pc.hddStorage}</td>
-                        <td>${pc.graphicsCard}</td>
-                        <td>${pc.avgPerformance}</td>
-                        <td>£${pc.price}</td>
-                    </tr>`;
+                    var hddStorage = pc.hddStorage !== null ? pc.hddStorage : "N/A";  // Check if HDD Storage is null
+
+                    var row = "<tr>" +
+                        "<td>" + pc.title + "</td>" +
+                        "<td>" + pc.cpu + "</td>" +
+                        "<td>" + pc.coreCount + "</td>" +
+                        "<td>" + pc.cpuBoostClock + "</td>" +
+                        "<td>" + pc.memory + "</td>" +
+                        "<td>" + pc.ssdStorage + "</td>" +
+                        "<td>" + hddStorage + "</td>" +
+                        "<td>" + pc.graphicsCard + "</td>" +
+                        "<td>" + pc.avgPerformance + "</td>" +
+                        "<td>" + "£" + pc.price + "</td>" +
+                        "</tr>";
 
                     pcTableBody.append(row);
+                    console.log("Populating table...");
                 }
             },
+            error: function() {
+                console.log("Error retrieving PC data.");
+            }
         });
     }
+
     // Call the populateTable function to initially populate the table
     populateTable();
 });
